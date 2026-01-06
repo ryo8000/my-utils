7fb51516f81f42dc545dfba90b0e71f5caa58c30
Ryo Hasegawa <49798519+ryo8000@users.noreply.github.com>
2026-01-06 13:10:03 +0000

    Add rule no-unused-vars to ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -41,6 +41,17 @@ export default tseslint.config(
         'error',
         { checksVoidReturn: { attributes: false } },
       ],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
       'no-console': 'warn',
       'simple-import-sort/imports': 'warn',
       'simple-import-sort/exports': 'warn',
