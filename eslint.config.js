1914d97540efa9d0844da8c42c4cfaeb5ba188fa
Ryo Hasegawa <49798519+ryo8000@users.noreply.github.com>
2026-06-09 20:53:10 +0900

    Remove @eslint/js dependency and update eslint config import

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,11 @@
-import eslint from '@eslint/js';
 import prettier from 'eslint-config-prettier';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import typescriptEslint from 'typescript-eslint';
 
-export default tseslint.config(
-  {
-    files: ['**/*.ts'],
-    ...eslint.configs.recommended,
-  },
-  ...tseslint.configs.recommendedTypeChecked,
-  ...tseslint.configs.stylisticTypeChecked,
+export default typescriptEslint.config(
+  ...typescriptEslint.configs.strictTypeChecked,
+  ...typescriptEslint.configs.stylisticTypeChecked,
   {
     files: ['**/*.ts'],
     languageOptions: {
