18e211a15eeba9b7577999897d62647e12b12c7e
Ryo Hasegawa <49798519+ryo8000@users.noreply.github.com>
2026-06-10 09:12:29 +0900

    Add strict TypeScript ESLint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,22 @@
 import prettier from 'eslint-config-prettier';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import globals from 'globals';
-import typescriptEslint from 'typescript-eslint';
+import tseslint from 'typescript-eslint';
 
-export default typescriptEslint.config(
-  ...typescriptEslint.configs.strictTypeChecked,
-  ...typescriptEslint.configs.stylisticTypeChecked,
+export default tseslint.config(
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
   {
     files: ['**/*.ts'],
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
+      parserOptions: {
+        projectService: true,
+      },
       globals: {
         ...globals.es2024,
         ...globals.node,
-      },
-      parserOptions: {
-        projectService: true,
       },
     },
     linterOptions: {
@@ -26,16 +26,27 @@ export default typescriptEslint.config(
       'simple-import-sort': simpleImportSort,
     },
     rules: {
-      '@typescript-eslint/consistent-type-definitions': ['warn', 'interface'],
+      '@typescript-eslint/consistent-type-definitions': [
+        'warn',
+        'interface'
+      ],
       '@typescript-eslint/consistent-type-imports': [
         'warn',
-        { prefer: 'type-imports', fixStyle: 'separate-type-imports' },
+        {
+          prefer: 'type-imports',
+          fixStyle: 'separate-type-imports'
+        },
       ],
       '@typescript-eslint/explicit-module-boundary-types': 'error',
       '@typescript-eslint/no-misused-promises': [
         'error',
-        { checksVoidReturn: { attributes: false } },
+        {
+          checksVoidReturn: {
+            attributes: false
+          }
+        },
       ],
+      '@typescript-eslint/no-shadow': 'error',
       '@typescript-eslint/no-unused-vars': [
         'error',
         {
@@ -47,7 +58,18 @@ export default typescriptEslint.config(
           caughtErrorsIgnorePattern: '^_',
         },
       ],
+      '@typescript-eslint/promise-function-async': 'error',
+      '@typescript-eslint/require-array-sort-compare': [
+        'error',
+        {
+          ignoreStringArrays: true
+        }
+      ],
+      '@typescript-eslint/switch-exhaustiveness-check': 'error',
+      curly: 'error',
+      eqeqeq: 'error',
       'no-console': 'warn',
+      'no-shadow': 'off',
       'simple-import-sort/imports': 'warn',
       'simple-import-sort/exports': 'warn',
     },
